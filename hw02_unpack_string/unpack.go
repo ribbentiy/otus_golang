e0bc5f481b0d6e2b3e645bc3a08f2824fc44f1fe
ribbentiy <ribbentiy@gmail.com>
2022-05-17 09:45:48 +0400

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,56 @@
-package hw02unpackstring
+package hw02_unpack_string
 
 import (
 	"errors"
+	"fmt"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(incomingString string) (string, error) {
+	var processingChar rune
+	var escapeFlag bool
+	var resultString strings.Builder
+
+	for _, char := range incomingString {
+		switch {
+		case char == 92:
+			if processingChar != 0 {
+				resultString.WriteRune(processingChar)
+				processingChar = 0
+			}
+			if escapeFlag == true {
+				processingChar = char
+				escapeFlag = false
+			} else {
+				escapeFlag = true
+			}
+		case unicode.IsDigit(char):
+			if escapeFlag {
+				processingChar = char
+				escapeFlag = false
+			} else if processingChar != 0 {
+				letter := fmt.Sprintf("%c", processingChar)
+				str := strings.Repeat(letter, int(char-48))
+				resultString.WriteString(str)
+				processingChar = 0
+			} else {
+				return "", ErrInvalidString
+			}
+		default:
+			escapeFlag = false
+			if processingChar != 0 {
+				resultString.WriteRune(processingChar)
+			}
+			processingChar = char
+
+		}
+
+	}
+	if processingChar != 0 {
+		resultString.WriteRune(processingChar)
+	}
+	return resultString.String(), nil
 }
